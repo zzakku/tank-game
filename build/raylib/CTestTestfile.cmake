34a0dea300258a3752ebcb6ae04fbc362394b8be
zzakku <165094009+zzakku@users.noreply.github.com>
2024-03-27 10:57:33 +0300

    Delete build directory

--- build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gato/Документы/RaylibApp/raylib
-# Build directory: /home/gato/Документы/RaylibApp/build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
