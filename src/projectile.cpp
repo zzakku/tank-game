8e8d177cae213dc8fecc12d1241ac2da09e405e5
Чиканчи Владислав <mr.vlad-2005@yandex.ru>
2024-05-21 06:36:04 +0300

    fix bullets

--- src/projectile.cpp
+++ src/projectile.cpp
@@ -11,12 +11,15 @@ void Projectile::Update()
         case left:
         pos_.x -= movespeed_;
         framerec_.x = static_cast<float>((graphics_.width/4)*4); 
+        break;
         case up:
         pos_.y -= movespeed_;
-        framerec_.x = static_cast<float>((graphics_.width/4)*1);  
+        framerec_.x = static_cast<float>((graphics_.width/4)*1);
+        break;  
         case down:
         pos_.y += movespeed_;
-        framerec_.x = static_cast<float>((graphics_.width/4)*3);            
+        framerec_.x = static_cast<float>((graphics_.width/4)*3);
+        break;            
     }
 
     hitbox_.x = pos_.x;
